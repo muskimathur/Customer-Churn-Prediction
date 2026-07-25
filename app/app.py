02851d219848881e9fdbfe1e852ed825851d9592
Muskan mathur <muskanmathur@Muskans-MacBook-Air.local>
2026-07-25 22:01:01 +0530

    Deployment changes

--- app/app.py
+++ app/app.py
@@ -133,5 +133,6 @@ def predict():
         return jsonify({"ok": False, "message": f"Prediction error: {e}"}), 200
 
 
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
